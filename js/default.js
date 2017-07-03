4773152904ff5042719c5a8a7f40b2f1ac2f61eb
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-07-03 17:15:05 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,6 +6,7 @@ var separate_time=function(time){
   return [sec,min,hours,days];
 }
 
+var update=function(){
 var now =new Date();
 var target=new Date(2020,7,24,0,0,0,0);
 var diff=target.getTime()-now.getTime();
@@ -15,3 +16,10 @@ document.getElementById('countdown').textContent=
   counter[2]+'時'+
   counter[1]+'分'+
   counter[0]+'秒'+;
+refresh();
+}
+
+var refresh=function(){
+  setTimeout(update,1000);
+}
+update();
