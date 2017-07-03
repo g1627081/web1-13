6170fe330a9436ba7f8179ce42e5d2552c428694
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-07-03 18:05:18 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -70,6 +70,7 @@ function getCookie(c_name){
   }
   return "";
 }
+
 var last_date=getCookie('lastDate');
 if(last_date){
   document.getElementById('cookie').textContent='前回訪れた時間:'+last_date;
@@ -78,3 +79,7 @@ if(last_date){
 }
 var current_time=new Date();
 setCookie('lastDate',current_time.toString(),7);
+
+document.getElementById('remove_cookie').onsubmit=function(){
+  setCookie('lastDate',"",0);
+};
